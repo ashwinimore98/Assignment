88e34a59be2841ccd52721062ba3b3c7948dd73d
ashwini more <more.ashg98@gmail.com>
2022-08-01 00:40:55 +0530

    factor commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace factor
+{
+    class Program
+    {
+      static void Main(string[] args)
+      {
+            Console.WriteLine("Plese provide input number");
+            int input = Int32.Parse(Console.ReadLine());
+       
+  
+            for(int i=1;i<=input;i++)
+            {
+                if(input%i==0)
+                {
+                    Console.WriteLine($"{i} is the factor of {input}");//1
+                }
+            }
+
+           
+      }
+        
+    }
+}
