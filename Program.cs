8f0b8a2dd32d4ff972b830790e988e2f466bb213
ashwini more <more.ashg98@gmail.com>
2022-07-31 19:54:30 +0530

    leap year

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NEW
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Leap year
+            int year;
+            Console.Write(" Enter Year = ");
+            year = int.Parse(Console.ReadLine());
+        
+        
+         
+            if(((year % 4 == 0) && (year %100 != 0)) || (year %400 == 0))
+            { 
+                Console.WriteLine(" Leap Year " + year);
+            }
+            else
+            {
+                Console.WriteLine(" Not a Leap Year " + year);
+            }
+            Console.ReadLine();
+        }
+    }
+}
