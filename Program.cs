642f22d1ffbe476db1d13fad173eed63a04390c9
ashwini more <more.ashg98@gmail.com>
2022-08-03 23:18:23 +0530

    day5one

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fibonacci_series
+{
+   
+    class Program
+    {
+      
+       
+        static void Main(string[] args)
+
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+
+            
+           
+           Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+             
+         
+            
+       
+           
+        }
+    }
+}
