32673b72a7c5bd9295bca9affd7c8770743de373
ashwini more <more.ashg98@gmail.com>
2022-08-15 22:49:32 +0530

    hormonic

---
+++ hormonic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hormonic
+{
+    namespace HARMONIC111
+    {
+        class Program
+
+        {
+            static void Main(string[] args)
+            {
+                HarmonicNumber(n: 3);
+
+            }
+            public static void HarmonicNumber(int n)//3
+            {
+
+                Double result = 0;//1
+                for (int i = 1; i <= n; i++)//i=3
+                {
+                    result = (result + 1) / i;//
+                }
+                Console.WriteLine($"Harmonic number= {result}");
+            }
+
+        }
+    }
+}
+
+
+
+
+
